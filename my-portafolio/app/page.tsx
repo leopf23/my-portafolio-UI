00d07317b2e0b1175cd3e19b86c97329d375257d
leonardopf <leonardoperezf23@gmail.com>
2024-07-20 13:32:31 -0400

    scroll snap top create

--- my-portafolio/app/page.tsx
+++ my-portafolio/app/page.tsx
@@ -6,17 +6,23 @@ export default function Home() {
     <main>
       <HeaderUi/>
       <div>
-{/* Img Banner Text */}
+    <div className="scroll-snap-container h-screen">
+      {/* Img Banner Text */}   
+        <div className="scroll-snap-item">
         <div className="bg-[url('/../back.svg')] bg-no-repeat bg-[length:86%_auto]  bg-[position:-34px_80px]">
-          <div className="w-full text-center p-5 md:p-40 h-[900px]">
-            <img alt="" src="../Text-home.svg" className="mx-auto h-[250px] w-auto" />
-            <img alt="" src="../scroll-down-icon.svg" className="relative top-28 mx-auto h-[40px] w-auto" />
+            <div className="w-full text-center p-5 md:p-40 h-[900px]">
+              <img alt="" src="../Text-home.svg" className="mx-auto h-[250px] w-auto" />
+              <img alt="" src="../scroll-down-icon.svg" className="relative top-28 mx-auto h-[40px] w-auto" />
+            </div>
           </div>
         </div>
-{/* description info*/}
-        <div className="p-36">
-          <p>leonardo</p>
+        {/* description info*/}
+        <div className="scroll-snap-item h-screen">
+          <div className="p-36">
+            <p>leonardo</p>
+          </div>
         </div>
+    </div>
       </div>
     </main>
   );
--- my-portafolio/tailwind.config.ts
+++ my-portafolio/tailwind.config.ts
@@ -7,7 +7,13 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+
     extend: {
+      extend:{
+        scrollSnapStop:{
+          always: 'always',
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
