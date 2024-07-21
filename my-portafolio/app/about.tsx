64d977bc1ea33398c11b94e3c91538b8e83dce60
leonardopf <leonardoperezf23@gmail.com>
2024-07-21 15:51:48 -0400

    scroll animateion add

---
+++ my-portafolio/app/about.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+        <div className='text-gray-500 text-lg'>
+          <span className='text-[#03249C]'>HELLO, </span> MY NAME IS
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
--- my-portafolio/app/page.tsx
+++ my-portafolio/app/page.tsx
@@ -1,32 +1,55 @@
+"use client"; // Marca el componente como Cliente
+import { useEffect } from "react";
 import HeaderUi from "./headerUi";
 import 'animate.css';
 import Banner from "./banner";
-
+import About from "./about";
 
 export default function Home() {
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show");
+        }
+      });
+    }, {
+      threshold: 0.1,
+    });
+
+    const items = document.querySelectorAll(".scroll-snap-item");
+    items.forEach(item => observer.observe(item));
+
+    return () => {
+      items.forEach(item => observer.unobserve(item));
+    };
+  }, []);
+
   return (
     <main>
-      <HeaderUi/>
+      <HeaderUi />
       <div>
-    <div className="scroll-snap-container h-screen">
-      {/* Img Banner Text */}   
-       <Banner/>
+        <div className="scroll-snap-container h-screen">
+          {/* Img Banner Text */}
+          <Banner />
 
-        {/* About me*/}
-        <div className="scroll-snap-item h-screen p-36">
-          <p className="text-[50px] font-semibold">About me</p>
-        </div>
+          {/* About me */}
+          <div className="scroll-snap-item h-screen md:p-36 p-10 animate-on-scroll ">
+            <About/>
+          </div>
 
-         {/* Skill*/}
-         <div className="scroll-snap-item h-screen p-36">
-          <p className="text-[50px] font-semibold">Skill</p>
-        </div>
+          {/* Skill */}
+          <div className="scroll-snap-item h-screen md:p-36 p-10 animate-on-scroll">
+            <p className="text-[50px] font-semibold">Skill</p>
+          </div>
 
-         {/*Portafolio*/}
-        <div className="scroll-snap-item h-screen p-36">
-          <p className="text-[50px] font-semibold">Portafolio</p>
+          {/* Portafolio */}
+          <div className="scroll-snap-item h-screen md:p-36 p-10 animate-on-scroll">
+            <p className="text-[50px] font-semibold">Portafolio</p>
+          </div>
         </div>
-    </div>
       </div>
     </main>
   );
