0b86520cea66632984b106e4bab785e5766adc76
leonardopf <leonardoperezf23@gmail.com>
2024-07-31 14:42:31 -0400

    header responsive dark model

--- my-portafolio/app/headerUi.tsx
+++ my-portafolio/app/headerUi.tsx
@@ -79,7 +79,7 @@ export default function HeaderUi() {
     </nav>
     <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
       <div className="fixed inset-0 z-10" />
-      <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+      <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white dark:bg-slate-700  px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
         <div className="flex items-center justify-between">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
@@ -105,7 +105,7 @@ export default function HeaderUi() {
                 <a
                   key={item.name}
                   href={item.href}
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 dark:text-white dark:hover:bg-slate-600 hover:bg-gray-50"
                 >
                   {item.name}
                 </a>
